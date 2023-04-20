f7fb2edfd194db78c6ed5709648245d0ed16816f
Anil Gündüz <67071978+anilgunduz@users.noreply.github.com>
2023-04-20 09:19:09 +0200

    Update main.R

--- main.R
+++ main.R
@@ -2,25 +2,27 @@ library(deepG)
 
 sequence <- microseq::readFasta("example.fasta")$Sequence
 
-#for testing a sequence without Ns, this mask 1000 nucleotides N:
-#indices <- sample(1:nchar(sequence), 1000)
+#for testing a sequence without Ns, this mask 5000 nucleotides N:
+#indices <- sample(1:nchar(sequence), 5000)
 #for (i in indices) {
 #  substring(sequence, i, 20000000L) <- "N"
 #}
 
-model <- keras::load_model_hdf5("model_imputation.hdf5", compile = FALSE)
+model <- keras::load_model_hdf5("/content/model_imputation_maxlen100.hdf5", compile = FALSE)
+
 len <- nchar(sequence)
 
+maxlen <- 100
 pred <- predict_model(vocabulary=c("a", "c", "g", "t", "n"),
                       output_format = "one_seq",
-                      model = model,
-                      layer_name = "flatten",
+                      model = model1,
+                      layer_name = "flatten_4",
                       path_input = "example.fasta",
                       sequence = sequence,
                       round_digits = 4,
                       filename = NULL,
-                      step = 1000,
-                      batch_size = 64,
+                      step = maxlen,
+                      batch_size = 512,
                       verbose = FALSE,
                       return_states = TRUE,
                       padding = "standard",
@@ -29,16 +31,16 @@ pred <- predict_model(vocabulary=c("a", "c", "g", "t", "n"),
 
 #this is for the last prediction, for example if the sequence is 2700 nt long, the first 2000 is classified using the function above
 #the last 700 is using this.
-if (len>1000){
+if (len>maxlen){
   pred1 <- predict_model(vocabulary=c("a", "c", "g", "t", "n"),
                       output_format = "one_seq",
-                      model = model,
-                      layer_name = "flatten",
+                      model = model1,
+                      layer_name = "flatten_4",
                       path_input = "example.fasta",
-                      sequence = substr(sequence, len-999, len),
+                      sequence = substr(sequence, len-maxlen+1, len),
                       round_digits = 4,
                       filename = NULL,
-                      step = 1000,
+                      step = maxlen,
                       batch_size = 4,
                       verbose = FALSE,
                       return_states = TRUE,
@@ -47,21 +49,23 @@ if (len>1000){
                       format = "fasta")
   k <- 0
 } else{
-  k <- (1000-len)*4
+  k <- (maxlen-len)*4
 }
-num_of_pred <- len %/% 1000
-last_pred <- num_of_pred * 1000 
-last_piece <- len %% 1000
-last_addition <- (1000 - last_piece)*4
+num_of_pred <- len %/% maxlen
+last_pred <- num_of_pred * maxlen 
+last_piece <- len %% maxlen
+last_addition <- (maxlen - last_piece)*4
 
 
 n_positions <- which(strsplit(sequence, "")[[1]] == "N")
 i=1 
+
+
 while (i<=length(n_positions)){
   j <- n_positions[i] 
-  j_rem_1000 <- ((j-1) %% 1000) +1
+  j_rem_maxlen <- ((j-1) %% maxlen) +1
   if  (j<=last_pred){
-    a = which.max(pred$state[((j-1)%/%1000)+1,(k+j_rem_1000*4-3):(k+j_rem_1000*4)])
+    a = which.max(pred$state[((j-1)%/%maxlen)+1,(k+j_rem_maxlen*4-3):(k+j_rem_maxlen*4)])
     if (a==1){
       substring(sequence, j) <- "A"
     }else if (a==2){
@@ -72,7 +76,7 @@ while (i<=length(n_positions)){
       substring(sequence, j) <- "T"
     } 
   } else {
-    a = which.max(pred1$state[1,(last_addition+j_rem_1000*4-3):(last_addition+j_rem_1000*4)])
+    a = which.max(pred1$state[1,(last_addition+j_rem_maxlen*4-3):(last_addition+j_rem_maxlen*4)])
     if (a==1){
       substring(sequence, j) <- "A"
     }else if (a==2){
@@ -86,7 +90,7 @@ while (i<=length(n_positions)){
   i <- i+1
 }
 
-#here is to test how many of mismatches there are out of 1000. there should be no Ns in your fasta file to test this
+#here is to test how many of mismatches there are out of 5000. there should be no Ns in your fasta file to test this
 #sequence1 <- microseq::readFasta("/content/example.fasta")$Sequence
 #num_mismatches <- sum(strsplit(sequence, "")[[1]] != strsplit(sequence1, "")[[1]])
 #cat("Number of mismatches: ", num_mismatches)
